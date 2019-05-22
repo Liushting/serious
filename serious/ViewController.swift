11ff88d6c55a10c4b998aa4458bd3a6777ab9547
LiuShTing <s86842222@gmail.com>
2019-05-22 19:59:23 +0800

    Initial Commit

---
+++ serious/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  serious
+//
+//  Created by 劉十六 on 2019/5/22.
+//  Copyright © 2019 Ting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
